5491bf2341d92a424ec9de75119d2c1f3f2b5e9d
Bowie <marketing@letscode.com.br>
2025-03-25 17:34:00 -0300

    AT2: atualização na fórmula

--- app/src/main/java/com/example/conta_barzinho/TelaContas.kt
+++ app/src/main/java/com/example/conta_barzinho/TelaContas.kt
@@ -3,6 +3,7 @@ package com.example.conta_barzinho
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.conta_barzinho.databinding.ActivityTelaContasBinding
+import com.google.android.material.snackbar.Snackbar
 
 class TelaContas : AppCompatActivity() {
 
@@ -16,21 +17,39 @@ class TelaContas : AppCompatActivity() {
 
         binding.btnCalcular.setOnClickListener {
 
-            val cervaValor: Float = binding.edittxValorcerva.text.toString().toFloat()
-            val cervaQtd: Int = binding.edittxtQtdcerva.text.toString().toInt()
-            val naoAlcolValor: Float = binding.edttextValornaoalcool.toString().toFloat()
-            val tiragValor: Float = binding.edttextValorTiragosto.toString().toFloat()
 
-            val nPessoas: Int = binding.edttextNpessoas.toString().toInt()
+            val tempCervaVal = binding.edittxValorcerva.text
+            val tempCervaQtd = binding.edittxtQtdcerva.text
+            val tempNaoAlcolVal = binding.edttextValornaoalcool.text
+            val tempTiragValue = binding.edttextValorTiragosto.text
+            val tempNPessoas = binding.edttextNpessoas.text
+
+            if ( tempCervaVal?.isEmpty() == true ||
+                tempCervaQtd?.isEmpty() == true ||
+                tempNaoAlcolVal?.isEmpty() == true ||
+                tempTiragValue?.isEmpty() == true ||
+                tempNPessoas?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(
+                        binding.edittxValorcerva,
+                        "Preencha todos os campos(mesmo com 0)",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+
+            } else {
 
 
+                val cervaValor: Float = binding.edittxValorcerva.text.toString().toFloat()
+                val cervaQtd: Int = binding.edittxtQtdcerva.text.toString().toInt()
+                val naoAlcolValor: Float = binding.edttextValornaoalcool.toString().toFloat()
+                val tiragValor: Float = binding.edttextValorTiragosto.toString().toFloat()
 
+                val nPessoas: Int = binding.edttextNpessoas.toString().toInt()
 
-
+            }
 
         }
-
-
 
     }
 }
